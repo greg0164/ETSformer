00ba614ce1fb7356bc079071aa4fec14ea9b0c55
greg0164 <135885441+greg0164@users.noreply.github.com>
2023-08-05 13:18:05 -0400

    Update Stock1.sh

--- scripts/Stock1.sh
+++ scripts/Stock1.sh
@@ -1,1 +1,1 @@
-python -u run.py --root_path ./dataset/Stock1/ --data_path stock1.csv --model_id stock1 --model ETSformer --data stock1 --features MS --target Close --freq d --seq_len 96 --pred_len 7 --e_layers 2 --d_layers 2 --enc_in 7 --dec_in 7 --c_out 7 --des 'Exp' --K 3 --learning_rate 1e-5 --itr 1
+python -u run.py --root_path ./dataset/Stock1/ --data_path stock1.csv --model_id stock1 --model ETSformer --data custom --features MS --target Close --freq d --seq_len 96 --pred_len 7 --e_layers 2 --d_layers 2 --enc_in 7 --dec_in 7 --c_out 7 --des 'Exp' --K 3 --learning_rate 1e-5 --itr 1
