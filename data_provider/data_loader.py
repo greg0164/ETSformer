86f8c079a47559fabb42637f56264d00e0b41fc0
greg0164 <135885441+greg0164@users.noreply.github.com>
2023-11-01 06:32:43 -0400

    Update data_loader.py

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -274,6 +274,7 @@ class Dataset_Custom(Dataset):
         
         # save off the sign of change column
         saved_column = df_data['Positive Return'].copy()
+        sc_index = df_data.columns.get_loc('Positive Return')
         if self.scale:
             train_data = df_data[border1s[0]:border2s[0]]
             self.scaler.fit(train_data.values)
@@ -281,7 +282,7 @@ class Dataset_Custom(Dataset):
         else:
             data = df_data.values
         # put back the raw sign of change column
-        data['Positive Return']=saved_column
+        data[:,sc_index]=saved_column.values
         
         df_stamp = df_raw[['date']][border1:border2]
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
